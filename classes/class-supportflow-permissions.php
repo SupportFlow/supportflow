77b0d94d04730e8bd856c4002ab274e34e872d1b
tollmanz <tollmanz@gmail.com>
2012-11-03 15:17:27 -0700

    Updated custom capabilities to better align with WordPress standards.
    In the initial capabilities work, the "others" capabilities were completely ignored. In order to properly determine who has access to what, these "others" capabilities need to be considered. The following 4 capabilities were added:
    
    * sf_close_others_threads
    * sf_open_others_threads
    * sf_reopen_others_threads
    * sf_comment_on_others_threads
    
    These 4 capabilities were added to the administrator and editor roles (and super admins also get these capabilities without any code changes).
    
    This commit also changed previous capabilities to make them plural (e.g., sf_open_thread was changed to sf_open_threads) to better align with WordPress conventions.
    
    See #55.

--- classes/class-supportflow-permissions.php
+++ classes/class-supportflow-permissions.php
@@ -7,36 +7,68 @@
 class SupportFlow_Permissions extends SupportFlow {
 
 	/**
-	 * Value of the "close thread" capability.
+	 * Value of the "close threads" capability.
 	 *
 	 * @since	0.1
 	 * @var		string
 	 */
-	private $_close_thread_capability = 'sf_close_thread';
+	private $_close_threads_capability = 'sf_close_threads';
 
 	/**
-	 * Value of the "open thread" capability.
+	 * Value of the "open threads" capability.
 	 *
 	 * @since	0.1
 	 * @var		string
 	 */
-	private $_open_thread_capability = 'sf_open_thread';
+	private $_open_threads_capability = 'sf_open_threads';
 
 	/**
-	 * Value of the "reopen thread" capability.
+	 * Value of the "reopen threads" capability.
 	 *
 	 * @since	0.1
 	 * @var		string
 	 */
-	private $_reopen_thread_capability = 'sf_reopen_thread';
+	private $_reopen_threads_capability = 'sf_reopen_threads';
 
 	/**
-	 * Value of the "comment on thread" capability.
+	 * Value of the "comment on threads" capability.
 	 *
 	 * @since	0.1
 	 * @var		string
 	 */
-	private $_comment_on_thread_capability = 'sf_comment_on_thread';
+	private $_comment_on_threads_capability = 'sf_comment_on_threads';
+
+	/**
+	 * Value of the "close others' threads" capability.
+	 *
+	 * @since	0.1
+	 * @var		string
+	 */
+	private $_close_others_threads_capability = 'sf_close_others_threads';
+
+	/**
+	 * Value of the "open others' threads" capability.
+	 *
+	 * @since	0.1
+	 * @var		string
+	 */
+	private $_open_others_threads_capability = 'sf_open_others_threads';
+
+	/**
+	 * Value of the "reopen others' threads" capability.
+	 *
+	 * @since	0.1
+	 * @var		string
+	 */
+	private $_reopen_others_threads_capability = 'sf_reopen_others_threads';
+
+	/**
+	 * Value of the "comment on others' threads" capability.
+	 *
+	 * @since	0.1
+	 * @var		string
+	 */
+	private $_comment_on_others_threads_capability = 'sf_comment_on_others_threads';
 
 	/**
 	 * Initiates the actions for the permissions class.
@@ -76,81 +108,135 @@ class SupportFlow_Permissions extends SupportFlow {
 		$admin = get_role( 'administrator' );
 
 		if ( null !== $admin ) {
-			$admin->add_cap( $this->get_close_thread_capability() );
-			$admin->add_cap( $this->get_open_thread_capability() );
-			$admin->add_cap( $this->get_reopen_thread_capability() );
-			$admin->add_cap( $this->get_comment_on_thread_capability() );
+			$admin->add_cap( $this->get_close_threads_capability() );
+			$admin->add_cap( $this->get_open_threads_capability() );
+			$admin->add_cap( $this->get_reopen_threads_capability() );
+			$admin->add_cap( $this->get_comment_on_threads_capability() );
+
+			$admin->add_cap( $this->get_close_others_threads_capability() );
+			$admin->add_cap( $this->get_open_others_threads_capability() );
+			$admin->add_cap( $this->get_reopen_others_threads_capability() );
+			$admin->add_cap( $this->get_comment_on_others_threads_capability() );
 		}
 
 		// Modify the Editor role to include close, open, reopen and comment on thread capabilities
 		$editor = get_role( 'editor' );
 
 		if ( null !== $editor ) {
-			$editor->add_cap( $this->get_close_thread_capability() );
-			$editor->add_cap( $this->get_open_thread_capability() );
-			$editor->add_cap( $this->get_reopen_thread_capability() );
-			$editor->add_cap( $this->get_comment_on_thread_capability() );
+			$editor->add_cap( $this->get_close_threads_capability() );
+			$editor->add_cap( $this->get_open_threads_capability() );
+			$editor->add_cap( $this->get_reopen_threads_capability() );
+			$editor->add_cap( $this->get_comment_on_threads_capability() );
+
+			$admin->add_cap( $this->get_close_others_threads_capability() );
+			$admin->add_cap( $this->get_open_others_threads_capability() );
+			$admin->add_cap( $this->get_reopen_others_threads_capability() );
+			$admin->add_cap( $this->get_comment_on_others_threads_capability() );
 		}
 
 		// Modify the Author role to include close, open, and comment on thread capabilities
 		$author = get_role( 'author' );
 
 		if ( null !== $author ) {
-			$author->add_cap( $this->get_close_thread_capability() );
-			$author->add_cap( $this->get_open_thread_capability() );
-			$author->add_cap( $this->get_comment_on_thread_capability() );
+			$author->add_cap( $this->get_close_threads_capability() );
+			$author->add_cap( $this->get_open_threads_capability() );
+			$author->add_cap( $this->get_comment_on_threads_capability() );
 		}
 
 		// Modify the Contributor role to include the comment on thread capability
 		$contributor = get_role( 'contributor' );
 
 		if ( null !== $contributor ) {
-			$contributor->add_cap( $this->get_comment_on_thread_capability() );
+			$contributor->add_cap( $this->get_comment_on_threads_capability() );
 		}
 	}
 
 	/**
-	 * Get the value of $_close_thread_capability.
+	 * Get the value of $_close_threads_capability.
 	 *
 	 * @since	0.1
 	 *
 	 * @return	string
 	 */
-	public function get_close_thread_capability() {
-		return $this->_close_thread_capability;
+	public function get_close_threads_capability() {
+		return $this->_close_threads_capability;
 	}
 
 	/**
-	 * Get the value of $_open_thread_capability.
+	 * Get the value of $_open_threads_capability.
 	 *
 	 * @since	0.1
 	 *
 	 * @return	string
 	 */
-	public function get_open_thread_capability() {
-		return $this->_open_thread_capability;
+	public function get_open_threads_capability() {
+		return $this->_open_threads_capability;
 	}
 
 	/**
-	 * Get the value of $_reopen_thread_capability.
+	 * Get the value of $_reopen_threads_capability.
 	 *
 	 * @since	0.1
 	 *
 	 * @return	string
 	 */
-	public function get_reopen_thread_capability() {
-		return $this->_reopen_thread_capability;
+	public function get_reopen_threads_capability() {
+		return $this->_reopen_threads_capability;
 	}
 
 	/**
-	 * Get the value of $_comment_on_thread_capability.
+	 * Get the value of $_comment_on_threads_capability.
 	 *
 	 * @since	0.1
 	 *
 	 * @return	string
 	 */
-	public function get_comment_on_thread_capability() {
-		return $this->_comment_on_thread_capability;
+	public function get_comment_on_threads_capability() {
+		return $this->_comment_on_threads_capability;
+	}
+
+	/**
+	 * Get the value of $_close_others_threads_capability.
+	 *
+	 * @since	0.1
+	 *
+	 * @return	string
+	 */
+	public function get_close_others_threads_capability() {
+		return $this->_close_others_threads_capability;
+	}
+
+	/**
+	 * Get the value of $_open_others_threads_capability.
+	 *
+	 * @since	0.1
+	 *
+	 * @return	string
+	 */
+	public function get_open_others_threads_capability() {
+		return $this->_open_others_threads_capability;
+	}
+
+	/**
+	 * Get the value of $_reopen_others_threads_capability.
+	 *
+	 * @since	0.1
+	 *
+	 * @return	string
+	 */
+	public function get_reopen_others_threads_capability() {
+		return $this->_reopen_others_threads_capability;
+	}
+
+	/**
+	 * Get the value of $_comment_on_others_threads_capability.
+	 *
+	 * @since	0.1
+	 *
+	 * @return	string
+	 */
+	public function get_comment_on_others_threads_capability() {
+		return $this->_comment_on_others_threads_capability;
 	}
 }
 
