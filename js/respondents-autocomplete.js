80b54cad2eca9b79cf4cbd8a4ece5fa4d9cf2b5e
Greg Johnson <greg@uponaweb.com>
2012-11-04 18:29:10 -0600

    adding comments

--- js/respondents-autocomplete.js
+++ js/respondents-autocomplete.js
@@ -1,5 +1,34 @@
 jQuery(document).ready(function($) {
-
 	
+	var getSearchTerm = function() {
+
+		// Input of the Respondents field can contain multiple addresses,
+		// we only want to search on the last address
+		var resps = $('#respondents').val().replace(" ", '').split(",");
+		var search_for = resps.pop();
+
+		return { "respondents":search_for, "api-action":'get-respondents' };
+		
+	}	
+
+	$('#respondents').autocomplete({
+
+		source: function( req, response ) {
+			
+			$.ajax({
+				url: SFRespondentsAc.ajax_url,
+				dataType: 'json',
+				data: getSearchTerm(),
+				success: function(data) {
+					console.log(data);
+				}
+			});
+
+		},
+		select: function( event, ui ) {
+			console.log(ui);
+		},
+		minLength: 2,
+	});
 
 });
