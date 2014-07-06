7792c9d410d477773bd01d8f25a7a27bdb01cacc
Varun Agrawal <Varun@VarunAgw.com>
2014-07-07 02:23:48 +0530

    E-Mail accounts were not shown in filter menu in all threads page
    This bug was caused by accidental removal of a line most probably in
    refactor branch

--- classes/class-supportflow-admin.php
+++ classes/class-supportflow-admin.php
@@ -172,7 +172,7 @@ class SupportFlow_Admin extends SupportFlow {
 
 
 		// Filter to specify E-Mail account
-
+		$email_accounts = SupportFlow()->extend->email_accounts->get_email_accounts( true );
 		echo "<select name='email_account' id='email_account' class='postform'>";
 		echo "<option value=''>" . __( 'Show All Accounts', 'supportflow' ) . "</option>";
 		foreach ( $email_accounts as $id => $email_account ) {
