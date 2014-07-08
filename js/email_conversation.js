e39581f1a737b994b8889d969f1843110f739b99
Varun Agrawal <Varun@VarunAgw.com>
2014-07-09 03:34:43 +0530

    Allow forwarding a thread to an E-Mail ID
    It covers:
    * A meta box in view thread page which allowes entering E-Mail ID to send E-Mails
    * Sending thread replies along with its details to these E-Mail ID's
    * Checking if user has permission and CSRF check
    * Validating E-Mail ID's before sending

---
+++ js/email_conversation.js
@@ -0,0 +1,33 @@
+jQuery(document).ready(function () {
+	// Send conversation to E-Mail ID's
+	jQuery('#email_conversation_submit').click(function (event) {
+		event.preventDefault();
+		var email_ids = jQuery('#email_conversation_to').val();
+
+		jQuery.ajax(ajaxurl, {
+			type      : 'post',
+			data      : {
+				action                   : 'sf_forward_conversation',
+				email_ids                : email_ids,
+				post_id                  : SFEmailConversation.post_id,
+				_email_conversation_nonce: SFEmailConversation._email_conversation_nonce,
+			},
+			beforeSend: function () {
+				jQuery('#email_conversation_to').prop('disabled', true);
+				jQuery('#email_conversation_submit').prop('disabled', true);
+				jQuery('#email_conversation_status').text(SFEmailConversation.sending_emails);
+			},
+			success   : function (content) {
+				jQuery('#email_conversation_status').html(content);
+			},
+			error     : function () {
+				jQuery('#email_conversation_status').text(SFEmailConversation.failed_sending);
+			},
+			complete  : function () {
+				jQuery('#email_conversation_to').prop('disabled', false);
+				jQuery('#email_conversation_submit').prop('disabled', false);
+			},
+		});
+
+	});
+});
