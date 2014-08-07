d12bc36630f0e7669e3f0a6850db432fb01938e4
Varun Agrawal <Varun@VarunAgw.com>
2014-08-07 16:23:45 +0530

    Adding a grunt watch task to continously monitor changed JS files

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,31 +1,40 @@
 module.exports = function (grunt) {
+	var js_files = [
+		'js/dashboard.js',
+		'js/email_accounts.js',
+		'js/email_conversation.js',
+		'js/permissions.js',
+		'js/predefined-replies.js',
+		'js/preferences.js',
+		'js/respondents-autocomplete.js',
+		'js/statistics.js',
+		'js/ticket_attachments.js',
+		'js/tickets.js',
+	];
+
 	grunt.initConfig({
 		uglify: {
 			options: {
 				banner: '/*! SupportFlow minified version */\n'
 			},
 			build  : {
-				src : [
-					'js/dashboard.js',
-					'js/email_accounts.js',
-					'js/email_conversation.js',
-					'js/permissions.js',
-					'js/predefined-replies.js',
-					'js/preferences.js',
-					'js/respondents-autocomplete.js',
-					'js/statistics.js',
-					'js/ticket_attachments.js',
-					'js/tickets.js',
-				],
+				src : js_files,
 				dest: 'js/supportflow.min.js'
 			}
-		}
+		},
+		watch : {
+			scripts: {
+				files: js_files,
+				tasks: ['uglify'],
+			},
+		},
 	});
 
-	// Load the plugin that provides the "uglify" task.
+	// Load the required plugins.
 	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
 
-	// Default task(s).
+	// Running these tasks by default
 	grunt.registerTask('default', ['uglify']);
 
 };
