c1535678da9a8380e42672ceef71174f2d01fc88
Varun Agrawal <Varun@VarunAgw.com>
2014-08-09 02:37:15 +0530

    Updated version to 0.3

--- supportflow.php
+++ supportflow.php
@@ -7,7 +7,7 @@ defined( 'ABSPATH' ) or die( "Cheatin' uh?" );
  * Plugin URI:  https://wordpress.org/plugins/supportflow/
  * Description: Reinventing how you support your customers.
  * Author:      Daniel Bachhuber, Varun Agrawal, Alex Mills, Andrew Spittle
- * Version:     0.3-beta3
+ * Version:     0.3
  *
  * Text Domain: supportflow
  * Domain Path: /languages/
@@ -146,7 +146,7 @@ class SupportFlow {
 
 		/** Version ***********************************************************/
 
-		$this->version = '0.3-beta3'; // SupportFlow version
+		$this->version = '0.3'; // SupportFlow version
 
 		/** Paths *************************************************************/
 
